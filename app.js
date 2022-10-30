614c4c5e8ac6ede7a33df04b75f16851c404551c
Vitaly Vegera <surewek@gmail.com>
2022-10-30 22:48:30 +0300

    refactor: add alt attributes according to extended rule 1.2

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="delete-img"
+    deleteButtonImg.alt="";
     deleteButton.appendChild(deleteButtonImg);
 
 
